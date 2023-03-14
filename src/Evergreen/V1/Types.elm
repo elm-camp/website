a539351c472ab75c9bda2771dbaffa1998071942
Mario Rogic <hello@mario.net.au>
2023-03-14 15:12:05 +0000

    Preparing for v1

---
+++ src/Evergreen/V1/Types.elm
@@ -0,0 +1,34 @@
+module Evergreen.V1.Types exposing (..)
+
+import Browser
+import Browser.Navigation
+import Url
+
+
+type alias FrontendModel =
+    { key : Browser.Navigation.Key
+    , message : String
+    }
+
+
+type alias BackendModel =
+    { message : String
+    }
+
+
+type FrontendMsg
+    = UrlClicked Browser.UrlRequest
+    | UrlChanged Url.Url
+    | NoOpFrontendMsg
+
+
+type ToBackend
+    = NoOpToBackend
+
+
+type BackendMsg
+    = NoOpBackendMsg
+
+
+type ToFrontend
+    = NoOpToFrontend
