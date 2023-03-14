905cec3ea0c34c3c7818872051237fe370719f82
Mario Rogic <hello@mario.net.au>
2023-03-14 15:11:24 +0000

    Initial commit, try Stripe pricing table

---
+++ src/Frontend.elm
@@ -0,0 +1,76 @@
+module Frontend exposing (..)
+
+import Browser exposing (UrlRequest(..))
+import Browser.Navigation as Nav
+import Html
+import Html.Attributes as Attr
+import Lamdera
+import Types exposing (..)
+import Url
+
+
+type alias Model =
+    FrontendModel
+
+
+app =
+    Lamdera.frontend
+        { init = init
+        , onUrlRequest = UrlClicked
+        , onUrlChange = UrlChanged
+        , update = update
+        , updateFromBackend = updateFromBackend
+        , subscriptions = \m -> Sub.none
+        , view = view
+        }
+
+
+init : Url.Url -> Nav.Key -> ( Model, Cmd FrontendMsg )
+init url key =
+    ( { key = key
+      , message = "Welcome to Lamdera! You're looking at the auto-generated base implementation. Check out src/Frontend.elm to start coding!"
+      }
+    , Cmd.none
+    )
+
+
+update : FrontendMsg -> Model -> ( Model, Cmd FrontendMsg )
+update msg model =
+    case msg of
+        UrlClicked urlRequest ->
+            case urlRequest of
+                Internal url ->
+                    ( model
+                    , Nav.pushUrl model.key (Url.toString url)
+                    )
+
+                External url ->
+                    ( model
+                    , Nav.load url
+                    )
+
+        UrlChanged url ->
+            ( model, Cmd.none )
+
+        NoOpFrontendMsg ->
+            ( model, Cmd.none )
+
+
+updateFromBackend : ToFrontend -> Model -> ( Model, Cmd FrontendMsg )
+updateFromBackend msg model =
+    case msg of
+        NoOpToFrontend ->
+            ( model, Cmd.none )
+
+
+view : Model -> Browser.Document FrontendMsg
+view model =
+    { title = ""
+    , body =
+        [ Html.node "stripe-pricing-table"
+            [ Attr.attribute "pricing-table-id" "prctbl_1MlWUcHHD80VvsjKINsykCtd"
+            , Attr.attribute "publishable-key" "pk_live_dIdCQ17pxxCWIeRJ5ZuJ4Ynm00FgRhZ4jR"
+            ]
+            []
+        ]
+    }
