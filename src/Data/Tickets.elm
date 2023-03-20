9f17d1aa4d16bf91a33a111fa825fd746a9e6b6e
Mario Rogic <hello@mario.net.au>
2023-03-20 12:34:15 +0000

    Add tickets with Stripe product IDs

---
+++ src/Data/Tickets.elm
@@ -0,0 +1,26 @@
+module Data.Tickets exposing (..)
+
+
+tickets =
+    [ campTicket, couplesCampTicket, campfireTicket ]
+
+
+campTicket =
+    { name = "Elm Camp Denmark 23 - Camp Ticket"
+    , description = "Ticket for 1 Person including: on-site accommodation with private ensuite for 2 nights, breakfast, lunch, tea & dinners included."
+    , stripeProductId = "prod_NWZ5JHXspU1l8p"
+    }
+
+
+couplesCampTicket =
+    { name = "Elm Camp Denmark 23 - Couples Camp Ticket"
+    , description = "Tickets for 2 people including: on-site accommodation for two people with private ensuite for 2 nights, breakfast, lunch, tea & dinners included."
+    , stripeProductId = "prod_NWZ8FJ1Ckl9fIc"
+    }
+
+
+campfireTicket =
+    { name = "Elm Camp Denmark 23 - Campfire Ticket"
+    , description = "Ticket for 1 Person including: breakfast, lunch, tea & dinners included. Access to castle grounds. No accommodation included."
+    , stripeProductId = "prod_NWZAQ3eQgK0XlF"
+    }
