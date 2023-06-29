57df951de30acf675ffe07e5cccdce91ca740dca
Mario Rogic <hello@mario.net.au>
2023-06-29 09:54:04 +0200

    Fix v50 types

--- src/Evergreen/V50/Types.elm
+++ src/Evergreen/V50/Types.elm
@@ -34,7 +34,7 @@ type FrontendMsg_
     | LiveScheduleMsg Evergreen.V50.LiveSchedule.Msg
 
 
-type alias TicketAvailability = 
+type alias TicketAvailability =
     { campTicket : Bool
     , couplesCampTicket : Bool
     , campfireTicket : Bool
@@ -43,32 +43,35 @@ type alias TicketAvailability =
 
 type TicketsEnabled
     = TicketsEnabled
-    | TicketsDisabled 
-    { adminMessage : String
-    }
+    | TicketsDisabled
+        { adminMessage : String
+        }
 
 
-type alias InitData2 = 
-    { prices : (AssocList.Dict (Evergreen.V50.Id.Id Evergreen.V50.Stripe.ProductId) 
-    { priceId : (Evergreen.V50.Id.Id Evergreen.V50.Stripe.PriceId)
-    , price : Evergreen.V50.Stripe.Price
-    })
+type alias InitData2 =
+    { prices :
+        AssocList.Dict
+            (Evergreen.V50.Id.Id Evergreen.V50.Stripe.ProductId)
+            { priceId : Evergreen.V50.Id.Id Evergreen.V50.Stripe.PriceId
+            , price : Evergreen.V50.Stripe.Price
+            }
     , slotsRemaining : TicketAvailability
     , ticketsEnabled : TicketsEnabled
     }
 
 
-type alias LoadingModel = 
+type alias LoadingModel =
     { key : Browser.Navigation.Key
     , now : Time.Posix
-    , window : (Maybe 
-    { width : Int
-    , height : Int
-    })
+    , window :
+        Maybe
+            { width : Int
+            , height : Int
+            }
     , route : Evergreen.V50.Route.Route
     , isOrganiser : Bool
-    , initData : (Maybe InitData2)
-    , audio : (Maybe (Result Audio.LoadError Audio.Source))
+    , initData : Maybe InitData2
+    , audio : Maybe (Result Audio.LoadError Audio.Source)
     }
 
 
@@ -78,59 +81,61 @@ type EmailResult
     | EmailFailed Http.Error
 
 
-type alias Order = 
-    { priceId : (Evergreen.V50.Id.Id Evergreen.V50.Stripe.PriceId)
+type alias Order =
+    { priceId : Evergreen.V50.Id.Id Evergreen.V50.Stripe.PriceId
     , submitTime : Time.Posix
     , form : Evergreen.V50.PurchaseForm.PurchaseFormValidated
     , emailResult : EmailResult
     }
 
 
-type alias PendingOrder = 
-    { priceId : (Evergreen.V50.Id.Id Evergreen.V50.Stripe.PriceId)
+type alias PendingOrder =
+    { priceId : Evergreen.V50.Id.Id Evergreen.V50.Stripe.PriceId
     , submitTime : Time.Posix
     , form : Evergreen.V50.PurchaseForm.PurchaseFormValidated
     , sessionId : Lamdera.SessionId
     }
 
 
-type alias Price2 = 
-    { priceId : (Evergreen.V50.Id.Id Evergreen.V50.Stripe.PriceId)
+type alias Price2 =
+    { priceId : Evergreen.V50.Id.Id Evergreen.V50.Stripe.PriceId
     , price : Evergreen.V50.Stripe.Price
     }
 
 
-type alias BackendModel = 
-    { orders : (AssocList.Dict (Evergreen.V50.Id.Id Evergreen.V50.Stripe.StripeSessionId) Order)
-    , pendingOrder : (AssocList.Dict (Evergreen.V50.Id.Id Evergreen.V50.Stripe.StripeSessionId) PendingOrder)
-    , expiredOrders : (AssocList.Dict (Evergreen.V50.Id.Id Evergreen.V50.Stripe.StripeSessionId) PendingOrder)
-    , prices : (AssocList.Dict (Evergreen.V50.Id.Id Evergreen.V50.Stripe.ProductId) Price2)
+type alias BackendModel =
+    { orders : AssocList.Dict (Evergreen.V50.Id.Id Evergreen.V50.Stripe.StripeSessionId) Order
+    , pendingOrder : AssocList.Dict (Evergreen.V50.Id.Id Evergreen.V50.Stripe.StripeSessionId) PendingOrder
+    , expiredOrders : AssocList.Dict (Evergreen.V50.Id.Id Evergreen.V50.Stripe.StripeSessionId) PendingOrder
+    , prices : AssocList.Dict (Evergreen.V50.Id.Id Evergreen.V50.Stripe.ProductId) Price2
     , time : Time.Posix
     , ticketsEnabled : TicketsEnabled
     }
 
 
-type alias LoadedModel = 
+type alias LoadedModel =
     { key : Browser.Navigation.Key
     , now : Time.Posix
-    , window : 
-    { width : Int
-    , height : Int
-    }
+    , window :
+        { width : Int
+        , height : Int
+        }
     , showTooltip : Bool
-    , prices : (AssocList.Dict (Evergreen.V50.Id.Id Evergreen.V50.Stripe.ProductId) 
-    { priceId : (Evergreen.V50.Id.Id Evergreen.V50.Stripe.PriceId)
-    , price : Evergreen.V50.Stripe.Price
-    })
-    , selectedTicket : (Maybe ((Evergreen.V50.Id.Id Evergreen.V50.Stripe.ProductId), (Evergreen.V50.Id.Id Evergreen.V50.Stripe.PriceId)))
+    , prices :
+        AssocList.Dict
+            (Evergreen.V50.Id.Id Evergreen.V50.Stripe.ProductId)
+            { priceId : Evergreen.V50.Id.Id Evergreen.V50.Stripe.PriceId
+            , price : Evergreen.V50.Stripe.Price
+            }
+    , selectedTicket : Maybe ( Evergreen.V50.Id.Id Evergreen.V50.Stripe.ProductId, Evergreen.V50.Id.Id Evergreen.V50.Stripe.PriceId )
     , form : Evergreen.V50.PurchaseForm.PurchaseForm
     , route : Evergreen.V50.Route.Route
     , showCarbonOffsetTooltip : Bool
     , slotsRemaining : TicketAvailability
     , isOrganiser : Bool
     , ticketsEnabled : TicketsEnabled
-    , backendModel : (Maybe BackendModel)
-    , audio : (Maybe Audio.Source)
+    , backendModel : Maybe BackendModel
+    , audio : Maybe Audio.Source
     , pressedAudioButton : Bool
     }
 
@@ -140,20 +145,12 @@ type FrontendModel_
     | Loaded LoadedModel
 
 
-type alias FrontendModel =(Audio.Model FrontendMsg_ FrontendModel_)
+type alias FrontendModel =
+    Audio.Model FrontendMsg_ FrontendModel_
 
 
-type alias BackendModel =
-    { orders : (AssocList.Dict (Evergreen.V50.Id.Id Evergreen.V50.Stripe.StripeSessionId) Order)
-    , pendingOrder : (AssocList.Dict (Evergreen.V50.Id.Id Evergreen.V50.Stripe.StripeSessionId) PendingOrder)
-    , expiredOrders : (AssocList.Dict (Evergreen.V50.Id.Id Evergreen.V50.Stripe.StripeSessionId) PendingOrder)
-    , prices : (AssocList.Dict (Evergreen.V50.Id.Id Evergreen.V50.Stripe.ProductId) Price2)
-    , time : Time.Posix
-    , ticketsEnabled : TicketsEnabled
-    }
-
-
-type alias FrontendMsg =(Audio.Msg FrontendMsg_)
+type alias FrontendMsg =
+    Audio.Msg FrontendMsg_
 
 
 type ToBackend
@@ -166,7 +163,7 @@ type BackendMsg
     = GotTime Time.Posix
     | GotPrices (Result Http.Error (List Evergreen.V50.Stripe.PriceData))
     | OnConnected Lamdera.SessionId Lamdera.ClientId
-    | CreatedCheckoutSession Lamdera.SessionId Lamdera.ClientId (Evergreen.V50.Id.Id Evergreen.V50.Stripe.PriceId) Evergreen.V50.PurchaseForm.PurchaseFormValidated (Result Http.Error ((Evergreen.V50.Id.Id Evergreen.V50.Stripe.StripeSessionId), Time.Posix))
+    | CreatedCheckoutSession Lamdera.SessionId Lamdera.ClientId (Evergreen.V50.Id.Id Evergreen.V50.Stripe.PriceId) Evergreen.V50.PurchaseForm.PurchaseFormValidated (Result Http.Error ( Evergreen.V50.Id.Id Evergreen.V50.Stripe.StripeSessionId, Time.Posix ))
     | ExpiredStripeSession (Evergreen.V50.Id.Id Evergreen.V50.Stripe.StripeSessionId) (Result Http.Error ())
     | ConfirmationEmailSent (Evergreen.V50.Id.Id Evergreen.V50.Stripe.StripeSessionId) (Result Http.Error Evergreen.V50.Postmark.PostmarkSendResponse)
     | ErrorEmailSent (Result Http.Error Evergreen.V50.Postmark.PostmarkSendResponse)
