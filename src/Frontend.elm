5befd0e563983a5b78094e59beb3f21fbab6e72e
katjam <katjamordaunt@gmail.com>
2024-02-15 23:46:26 +0000

    Remove 2024 label from organiser section

--- src/Frontend.elm
+++ src/Frontend.elm
@@ -1140,7 +1140,7 @@ Last year, we were able to offer opportunity grants to cover both ticket and tra
 
 **Thanks to Concentric and generous individual sponsors for making the Elm Camp 2023 opportunity grants possible**.
 
-# 2024 Organisers
+# Organisers
 
 Elm Camp is a community-driven non-profit initiative, organised by enthusiastic members of the Elm community.
 
