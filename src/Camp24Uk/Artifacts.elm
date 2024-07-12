49fec13d34753fe937d7469a08bc18c3bb9b22fe
Mario Rogic <hello@mario.net.au>
2024-07-13 09:17:58 +1000

    Add Elm Town podcast

--- src/Camp24Uk/Artifacts.elm
+++ src/Camp24Uk/Artifacts.elm
@@ -16,6 +16,10 @@ This page is [open to contributions on Github](https://github.com/elm-camp/websi
 - [Elm Camp 2024 Reflections](https://wolfgangschuster.wordpress.com/2024/06/23/elm-camp-2024-reflections/) by @wolfadex (Blog)
 - [Elm Camp 2024](https://jfmengels.net/elm-camp-2024/) by @jfmengels (Blog)
 
+## Podcasts
+
+- [Elm Town 78: Elm Camp 2024 with Katja Mordaunt and Wolfgang Schuster](https://elm.town/episodes/elm-town-78-elm-camp-2024-with-katja-mordaunt-and-wolfgang-schuster)
+
 ## Media
 
 <img src="/24-colehayes/elm-camp-24-attendees.jpg" width="100%" />
