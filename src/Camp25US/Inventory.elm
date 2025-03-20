37caf6079a8b6585b9c2c4e6e2594868ee8ef33a
Tristan Pendergrass <tristanp@dropbox.com>
2025-03-19 17:06:57 -0700

    Add inventory, product, and ticket modules for 2025 conference. Implement accommodation types, ticket availability logic, and sponsorship details.

---
+++ src/Camp25US/Inventory.elm
@@ -0,0 +1,111 @@
+module Camp25US.Inventory exposing (..)
+
+import AssocList
+import Camp25US.Product as Product
+import PurchaseForm exposing (..)
+import Types exposing (..)
+
+
+maxAttendees =
+    80
+
+
+maxForAccommodationType t =
+    case t of
+        Offsite ->
+            -- Effectively no limit, the attendee limit should hit first
+            80
+
+        Campsite ->
+            20
+
+        Single ->
+            6
+
+        Double ->
+            15
+
+        Group ->
+            4
+
+
+slotsRemaining : BackendModel -> TicketAvailability
+slotsRemaining model =
+    let
+        bookedAccommodations =
+            model.orders |> extract (\id order -> order.form.accommodationBookings)
+
+        pendingAccommodations =
+            model.pendingOrder |> extract (\id order -> order.form.accommodationBookings)
+
+        bookedCountFor accomodationType =
+            bookedAccommodations
+                |> List.filter (\t -> t == accomodationType)
+                |> List.length
+
+        bookedAndPendingCountFor accomodationType =
+            (bookedAccommodations ++ pendingAccommodations)
+                |> List.filter (\t -> t == accomodationType)
+                |> List.length
+
+        remainingCountForAccommodation accommodationType =
+            maxForAccommodationType accommodationType - bookedCountFor accommodationType
+
+        bookedAttendees =
+            model.orders
+                |> extract (\id order -> order.form.attendees)
+                |> List.length
+
+        remainingCapacity =
+            maxAttendees - bookedAttendees
+    in
+    if remainingCapacity > 0 then
+        { attendanceTickets = remainingCapacity > 0
+        , campingSpots = remainingCountForAccommodation Campsite > 0
+        , singleRooms = remainingCountForAccommodation Single > 0
+        , doubleRooms = remainingCountForAccommodation Double > 0
+        , groupRooms = remainingCountForAccommodation Group > 0
+        }
+
+    else
+        -- We've reached capacity, no further tickets are available regardless of inventory
+        { attendanceTickets = False
+        , campingSpots = False
+        , singleRooms = False
+        , doubleRooms = False
+        , groupRooms = False
+        }
+
+
+allSoldOut : TicketAvailability -> Bool
+allSoldOut { attendanceTickets } =
+    attendanceTickets
+
+
+extract selector assocList =
+    assocList
+        |> AssocList.map selector
+        |> AssocList.toList
+        |> List.map Tuple.second
+        |> List.concat
+
+
+purchaseable : String -> TicketAvailability -> Bool
+purchaseable productId availability =
+    caseof productId
+        [ ( Product.ticket.attendanceTicket, availability.attendanceTickets )
+        , ( Product.ticket.offsite, availability.campingSpots )
+        , ( Product.ticket.campingSpot, availability.campingSpots )
+        , ( Product.ticket.singleRoom, availability.singleRooms )
+        , ( Product.ticket.doubleRoom, availability.doubleRooms )
+        , ( Product.ticket.groupRoom, availability.groupRooms )
+        ]
+
+
+caseof v opts =
+    case List.head (List.filter (\( a, b ) -> a == v) opts) of
+        Just ( a, b ) ->
+            b
+
+        Nothing ->
+            False
---
+++ src/Camp25US/Product.elm
@@ -0,0 +1,96 @@
+module Camp25US.Product exposing (..)
+
+import Env
+
+
+year =
+    "2025"
+
+
+ticket :
+    { attendanceTicket : String
+    , offsite : String
+    , campingSpot : String
+    , singleRoom : String
+    , doubleRoom : String
+    , groupRoom : String
+    }
+ticket =
+    case Env.mode of
+        Env.Production ->
+            { attendanceTicket = "prod_PhhIapmx1JVmy9"
+            , offsite = "prod_PiNYzP0oSVQgSD"
+            , campingSpot = "prod_PhhJG27yB8GmzH"
+            , singleRoom = "prod_PhhLtqV8I5Spjo"
+            , doubleRoom = "prod_PhhLZ9pJjyEDfF"
+            , groupRoom = "prod_PhhLsuxKXAfjse"
+            }
+
+        Env.Development ->
+            { attendanceTicket = "prod_PhhIapmx1JVmy9"
+            , offsite = "prod_PiNYzP0oSVQgSD"
+            , campingSpot = "prod_PhhJG27yB8GmzH"
+            , singleRoom = "prod_PhhLtqV8I5Spjo"
+            , doubleRoom = "prod_PhhLZ9pJjyEDfF"
+            , groupRoom = "prod_PhhLsuxKXAfjse"
+            }
+
+
+sponsorship : { silver : String, gold : String, platinum : String }
+sponsorship =
+    case Env.mode of
+        Env.Production ->
+            { silver = "prod_PmxuGnMiIu7Lb7"
+            , gold = "prod_PmxuoGoPc9guET"
+            , platinum = "prod_Pmxv076S7IM05N"
+            }
+
+        Env.Development ->
+            { silver = "prod_PmxuGnMiIu7Lb7"
+            , gold = "prod_PmxuoGoPc9guET"
+            , platinum = "prod_Pmxv076S7IM05N"
+            }
+
+
+type alias Sponsorship =
+    { name : String, price : Int, productId : String, description : String, features : List String }
+
+
+sponsorshipItems =
+    [ { name = "Silver"
+      , price = 750
+      , productId = sponsorship.silver
+      , description = "You will be a major supporter of Elm Camp " ++ year ++ "."
+      , features =
+            [ "Thank you tweet"
+            , "Logo on webpage"
+            , "Small logo on shared slide, displayed during breaks"
+            ]
+      }
+    , { name = "Gold"
+      , price = 1500
+      , productId = sponsorship.gold
+      , description = "You will be a pivotal supporter of Elm Camp " ++ year ++ "."
+      , features =
+            [ "Thank you tweet"
+            , "Rollup or poster inside the venue (provided by you)"
+            , "Logo on webpage"
+            , "Medium logo on shared slide, displayed during breaks"
+            , "1 free campfire ticket"
+            ]
+      }
+    , { name = "Platinum"
+      , price = 3000
+      , productId = sponsorship.platinum
+      , description = "You will be principal sponsor and guarantee that Elm Camp " ++ year ++ " is a success."
+      , features =
+            [ "Thank you tweet"
+            , "Rollup or poster inside the venue (provided by you)"
+            , "Self-written snippet on shared web page about use of Elm at your company"
+            , "Logo on webpage"
+            , "2 free campfire tickets or 1 free ticket with accommodation"
+            , "Big logo on shared slide, displayed during breaks"
+            , "Honorary mention in opening and closing talks"
+            ]
+      }
+    ]
