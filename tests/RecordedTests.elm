ed22d5ef4463308407073cfd77058771e7b109f2
Martin Stewart <MartinSStewart@gmail.com>
2026-02-18 23:46:42 +0100

    wip

--- tests/RecordedTests.elm
+++ tests/RecordedTests.elm
@@ -80,6 +80,11 @@ handleHttpRequests overrides fileData { currentRequest } =
                     { url = currentRequest.url, statusCode = 200, statusText = "OK", headers = Dict.empty }
                     ("""{"id":\"""" ++ stripeSessionId ++ "\"}")
 
+            else if currentRequest.url == "https://open.er-api.com/v6/latest/CZK" then
+                StringHttpResponse
+                    { url = currentRequest.url, statusCode = 200, statusText = "OK", headers = Dict.empty }
+                    exchangeRateResponse
+
             else
                 UnhandledHttpRequest
 
@@ -491,3 +496,8 @@ stripePricesResponse =
   "has_more": true,
   "url": "/v1/prices"
 }"""
+
+
+exchangeRateResponse : String
+exchangeRateResponse =
+    """{"result":"success","provider":"https://www.exchangerate-api.com","documentation":"https://www.exchangerate-api.com/docs/free","terms_of_use":"https://www.exchangerate-api.com/terms","time_last_update_unix":1771372952,"time_last_update_utc":"Wed, 18 Feb 2026 00:02:32 +0000","time_next_update_unix":1771461262,"time_next_update_utc":"Thu, 19 Feb 2026 00:34:22 +0000","time_eol_unix":0,"base_code":"CZK","rates":{"CZK":1,"AED":0.17914,"AFN":3.099372,"ALL":3.973075,"AMD":18.418392,"ANG":0.087314,"AOA":45.708659,"ARS":70.838759,"AUD":0.069102,"AWG":0.087314,"AZN":0.082964,"BAM":0.080573,"BBD":0.097557,"BDT":5.975498,"BGN":2.711751,"BHD":0.018341,"BIF":144.61165,"BMD":0.048779,"BND":0.061624,"BOB":0.338482,"BRL":0.254623,"BSD":0.048779,"BTN":4.420903,"BWP":0.657228,"BYN":0.138802,"BZD":0.097557,"CAD":0.066567,"CDF":111.992481,"CHF":0.037579,"CLF":0.001066,"CLP":42.115397,"CNH":0.336572,"CNY":0.3408,"COP":179.002488,"CRC":23.512248,"CUP":1.170687,"CVE":4.542538,"DJF":8.668986,"DKK":0.307342,"DOP":3.03193,"DZD":6.332679,"EGP":2.292068,"ERN":0.731679,"ETB":7.591743,"EUR":0.041197,"FJD":0.106122,"FKP":0.035962,"FOK":0.307342,"GBP":0.035991,"GEL":0.130731,"GGP":0.035962,"GHS":0.537601,"GIP":0.035962,"GMD":3.614406,"GNF":426.774642,"GTQ":0.374398,"GYD":10.216049,"HKD":0.381113,"HNL":1.291611,"HRK":0.310395,"HTG":6.389961,"HUF":15.580769,"IDR":821.875524,"ILS":0.15115,"IMP":0.035962,"INR":4.42477,"IQD":63.927039,"IRR":62555.023923,"ISK":5.973146,"JEP":0.035962,"JMD":7.633807,"JOD":0.034584,"JPY":7.475499,"KES":6.292519,"KGS":4.268347,"KHR":195.986842,"KID":0.069102,"KMF":20.267376,"KRW":70.402584,"KWD":0.014932,"KYD":0.040649,"KZT":23.929516,"LAK":1054.070902,"LBP":4365.686985,"LKR":14.960643,"LRD":9.082726,"LSL":0.782336,"LYD":0.307697,"MAD":0.445302,"MDL":0.827836,"MGA":212.785714,"MKD":2.538274,"MMK":102.540461,"MNT":172.799837,"MOP":0.392546,"MRU":1.944771,"MUR":2.241378,"MVR":0.754392,"MWK":85.013701,"MXN":0.837244,"MYR":0.190209,"MZN":3.110562,"NAD":0.782336,"NGN":66.035991,"NIO":1.796596,"NOK":0.465188,"NPR":7.073445,"NZD":0.080855,"OMR":0.018755,"PAB":0.048779,"PEN":0.163506,"PGK":0.209586,"PHP":2.821394,"PKR":13.678831,"PLN":0.173725,"PYG":318.255094,"QAR":0.177554,"RON":0.209975,"RSD":4.83123,"RUB":3.746487,"RWF":71.335275,"SAR":0.18292,"SBD":0.382822,"SCR":0.705823,"SDG":21.808199,"SEK":0.437933,"SGD":0.061624,"SHP":0.035962,"SLE":1.200888,"SLL":1200.894902,"SOS":27.841121,"SRD":1.840025,"SSP":223.672714,"STN":1.009316,"SYP":5.480602,"SZL":0.782336,"THB":1.524727,"TJS":0.45803,"TMT":0.170813,"TND":0.139306,"TOP":0.113761,"TRY":2.132472,"TTD":0.331135,"TVD":0.069102,"TWD":1.531448,"TZS":127.566021,"UAH":2.11254,"UGX":172.684781,"USD":0.048779,"UYU":1.894314,"UZS":593.855413,"VES":19.334279,"VND":1265.671705,"VUV":5.690396,"WST":0.129086,"XAF":27.023168,"XCD":0.131702,"XCG":0.087314,"XDR":0.03548,"XOF":27.023168,"XPF":4.916067,"YER":11.634724,"ZAR":0.782337,"ZMW":0.901461,"ZWG":1.248254,"ZWL":1.248246}}"""
